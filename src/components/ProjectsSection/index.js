a881a38fe532175c2e61e1df0425d2fe24b73de0
GabrielOJorge <goliver331@protonmail.com>
2022-08-05 15:47:10 -0300

    create ProjectsSection component

---
+++ src/components/ProjectsSection/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class ProjectsSection extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  render() {
+    return (
+      <section className="projects-section">
+        <h1>Projects</h1>
+
+        <form action="#" onSubmit={this.handleSubmit}>
+          <input type="text" name="title" id="title" placeholder="Title"/>
+          <input type="text" name="technologies" id="technologies" placeholder="Technologies"/>
+          <textarea name="project-description" id="project-description" placeholder="Project description"></textarea>
+          <button type="submit" className="main-btn">Add</button>
+        </form>
+      </section>
+    );
+
+  };
+};
+
+export default ProjectsSection;
